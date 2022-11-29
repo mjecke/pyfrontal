35ccd07314dcda2400d74a9ef72811326d13e287
Mike <40167808+mjecke@users.noreply.github.com>
2022-11-29 18:24:16 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+This is a self-contained python script that can be used to compute frontal area
+projections of STL meshes for use in analysis, particularly when doing 
+comparisons of frontal area of automotive shapes for CFD purposes. 
+
+There is a formal python package named parea 
+(https://github.com/nathanrooy/p-area) that takes an analytical approach to this 
+problem by calculating the area of all of the triangles of a mesh in the chosen 
+axis, however this program was very, very slow for me when attempting 
+computations on very large STL files which is common when dealing with meshes 
+straight from 3D scanning, say, of wind tunnel models. 
+'parea' could not handle computing meshes that were GB's in size without locking 
+up a machine. 
+The method I am using leverages the VTK rendering pipeline to take a numerical 
+approach to the area calculation problem, using ray-tracing on a fixed raster 
+resolution window to compute an approximation of the projected ortogonal area.
+
+This method is ~30x-60x faster than the analytical approach and is inspired by
+the  method that is used to measure frontal area of physical vehicles for 
+windtunnel testing, which uses collimated laser light sources and photo mapping 
+to measure the shadow of a vehicle in a darkroom.  
+
+Because the heavy lifting is being done by VTK, a compiled package that is 
+meant to load and view very large data and visualizations, this script can 
+handle medium sized exterior vehicle STL's in <1 second and can handle even very
+large scans, 1GB+ files in about 30 seconds on an M1 Macbook.  
+Binary STLs load fine and the VTK render doesn't care about unified
+normals, non-manifolds, etc.  It tends to just work.
+
+The accuracy for typical sedan sized test shapes seems to be roughly ~0.0015 m^2
+or better depending on the resolution used used and the fit factor of vehicle to
+render frame.  
+
+The program has several command line options (see -h for help) to help get best
+results.  It generates a PNG of the computed shadowmask by default and shows it
+in a preview, but this can be disabled via command line arguments. Default is 
+projection in the X direction as is the case for most frontal areas of 
+automotive models and default windowing and resolution is roughly correct for 
+most vehicles. 
+
+The scale and resolution is adaptable but is mostly setup to work with car-
+sized STL models in millimeters.  The program has some safety checks to 
+determine if the renderer is configured to work with an appropriate model size,
+and if not, it exits with error info. 
+
+An example use case: 
---
+++ pyfrontal.py
@@ -0,0 +1,281 @@
+################################################################################
+# The following is a python script that uses VTK to generate fast
+# projections of STL meshes for frontal area calculations.  See README for more
+# information.
+# (c) 2022 M. Eckert 
+################################################################################
+import os
+import time
+
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy
+import numpy as np
+
+import argparse
+
+# ------------------ Argument Parsing ------------------------------------------
+def valid_file(param):
+    base, ext = os.path.splitext(param)
+    if ext.lower() not in ('.stl'):
+        raise argparse.ArgumentTypeError(
+            '\n\nERROR: File must be an STL mesh.\n')
+    return param
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("filename", type=valid_file, help="Filename of STL mesh")
+parser.add_argument("-noshow", help="Skip graphic window after render",
+                    action="store_true")
+parser.add_argument("-nosave", help="Skip PNG outut of render",
+                    action="store_true")
+parser.add_argument("-x", "-X", help="Sets X as proj. normal axis (default)",
+                    action="store_true")
+parser.add_argument("-y", "-Y", help="Sets Y as proj. normal axis",
+                    action="store_true")
+parser.add_argument("-z", "-Z", help="Sets Z as proj normal axis",
+                    action="store_true")
+parser.add_argument("-fitfactor", help="Factor to help fit view to model",
+                    type=float, default=1.0)
+parser.add_argument("-res", help="Render Window Resolution (default 1500px)",
+                    type=int, default=1500)
+parser.add_argument("-ground", type=int, default=-10000,
+                    help="Ground height clipping in mm from Z=0")
+parser.add_argument("-debug", help="Turns on debug messages",
+                    action="store_true")
+
+args = parser.parse_args()
+
+if (args.x + args.y + args.z) > 1: #ensure only one axis is used
+    print("\nERROR: Choose only one projection axis argument.\n")
+    quit()
+
+# debug print function enable/disable 
+if args.debug:
+    printd = print #it just turns printd functions to print statements
+else:
+    def printd(x):
+        pass #else ignore them
+
+projaxis = "X" #default axis to X
+if args.x:
+    projaxis = "X"
+if args.y:
+    projaxis = "Y"
+if args.z:
+    projaxis = "Z"
+
+# Ground height in mm from Z=0
+ground = (args.ground)
+
+#fit factor is a scaling to help fit to scale inside view window. usually needs
+#to be within 1.0 to 2.0 for cars <-> trucks
+fitfactor = (args.fitfactor)
+
+printd("\n")
+printd("DEBUG ON")
+printd("----------------------------------------------------------------------")
+
+printd("FITFACTOR = " +str(fitfactor))
+# ------------------ VTK Render Setup ------------------------------------------
+max_frame = args.res #sets max resolution, render windows size
+
+# Create the renderer
+ren = vtk.vtkRenderer()
+
+# Create the window
+renWin = vtk.vtkRenderWindow()
+
+# Add the renderer to the window
+renWin.AddRenderer(ren)
+
+# Define an interactor. 
+iren = vtk.vtkRenderWindowInteractor()
+
+# Set the window defined above as the window that the interactor
+# will work on.
+iren.SetRenderWindow(renWin)
+
+# Initialize the object that is used to load the .stl file
+stlFileReader = vtk.vtkSTLReader()
+
+# Specify the .stl file's name.
+stlFileReader.SetFileName(args.filename)
+
+# Load the .stl file.
+stlFileReader.Update()
+
+# Get polydata from the 'stlFileReader' variable as new named 'surfacePolyData'
+surfacePolyData = stlFileReader.GetOutput()
+
+triangles_count = surfacePolyData.GetNumberOfCells()
+
+# Clipping plane for ground
+plane = vtk.vtkPlane()
+plane.SetNormal(0, 0, 1)
+plane.SetOrigin(0, 0, ground)
+
+clip = vtk.vtkClipPolyData()
+clip.SetClipFunction(plane)
+clip.SetInputData(surfacePolyData)
+clip.Update()
+surfacePolyData = clip.GetOutput(0)
+
+# Get Boundary of resultant data
+bounds = surfacePolyData.GetBounds()
+
+x_bounds = bounds[0:2]
+y_bounds = bounds[2:4]
+z_bounds = bounds[4:6]
+
+printd("Bounds in x = " +str(x_bounds))
+printd("Bounds in y = " +str(y_bounds))
+printd("Bounds in z = " +str(z_bounds))
+
+intXdim = int(abs(x_bounds[0])+abs(x_bounds[1]))
+intYdim = int(abs(y_bounds[0])+abs(y_bounds[1]))
+intZdim = int(abs(z_bounds[0])+abs(z_bounds[1]))
+
+printd("model X Dimension = " +str(intXdim))
+printd("model Y Dimension = " +str(intYdim))
+printd("model Z Dimension = " +str(intZdim))
+
+# Check for model dimensions that aren't going to work.  
+if (intXdim or intYdim or intZdim) < 100:
+    print("Model dimensions small, methods likely innacurate.")
+    print("Program meant for vehicle models in mm units.")
+    print("Program exiting...")
+    quit()
+
+if (intXdim or intYdim or intZdim) > 30000:
+    print("Model dimensions very large, methods likely inoperable.")
+    print("Program exiting...")
+    quit()
+
+if projaxis == "X":
+    frame_width = (intYdim)
+    frame_height = (intZdim)
+
+if projaxis == "Y":
+    frame_width = (intXdim)
+    frame_height = (intZdim)
+    
+if projaxis == "Z":
+    frame_width = (intXdim)
+    frame_height = (intYdim)
+    
+minscale = min([frame_width, frame_height])
+maxscale = max([frame_width, frame_height])
+
+aspect = frame_width/frame_height
+
+printd("ASPECT RATIO = " + str(aspect))
+printd("FRAME WIDTH " + str(frame_width))
+printd("FRAME HEIGHT " + str(frame_height))
+
+# Find center
+x_center = (int(np.average(x_bounds)))
+y_center = (int(np.average(y_bounds)))
+z_center = (int(np.average(z_bounds)))
+
+printd("X CENTER = " + str(x_center))
+printd("Y CENTER = " + str(y_center))
+printd("Z CENTER = " + str(z_center))
+
+# Initialize the mapper
+surfaceMapper = vtk.vtkPolyDataMapper()
+surfaceMapper.SetInputData(surfacePolyData)
+
+# Initialize the actor
+surfaceActor = vtk.vtkActor()
+surfaceActor.SetMapper(surfaceMapper)
+
+# Render projection all white
+surfaceActor.GetProperty().SetColor(255, 255, 255)
+
+# Add the actor to the renderer
+ren.AddActor(surfaceActor)
+
+# Interactor initialize
+iren.Initialize()
+camera = ren.GetActiveCamera()
+camera.ParallelProjectionOn()
+
+camera.SetParallelScale(max_frame*fitfactor) #fitfactor used here
+
+camera.SetClippingRange(-100000, 100000) #really large so no clipping
+
+# Set up view on center of data
+if projaxis == "X":
+    camera.SetViewUp(0, 0, 1)
+    camera.SetPosition((x_center + 1), y_center, z_center)
+
+if projaxis == "Y":
+    camera.SetViewUp(0, 0, 1)
+    camera.SetPosition(x_center, (y_center + 1), z_center)
+
+if projaxis == "Z":
+    camera.SetViewUp(0, 1, 0)
+    camera.SetPosition(x_center, y_center, (z_center + 1))
+
+
+camera.SetFocalPoint(x_center, y_center, z_center)
+
+grabber = vtk.vtkWindowToImageFilter()
+grabber.SetInput(renWin)
+renWin.SetSize(int(max_frame*aspect), max_frame)
+grabber.Update()
+
+if args.nosave is False: # Save the resulting render to a file for safekeeping
+    writer = vtk.vtkPNGWriter()
+    writer.SetInputData(grabber.GetOutput())
+    writer.SetFileName(os.path.splitext(args.filename)[0]  + "_output.png")
+    writer.Write()
+
+img = grabber.GetOutput()
+rows, cols, _ = img.GetDimensions()
+img = vtk_to_numpy(img.GetPointData().GetScalars())
+img = img.reshape(cols, rows, -1)
+
+img = (np.dot(img[...,:3], [1/3, 1/3, 1/3])).astype(int) #rgb to mono
+
+imgEdges = np.sum(img[0,:])+np.sum(img[-1,:])+np.sum(img[:,0])+np.sum(img[:,-1])
+
+if imgEdges > 0:
+    print("\n")
+    print("Model is not fit to render window, results cannot be computed")
+    print("accurately.  Change argument -fitfactor to a greater value.")
+    print("Program exiting...")
+    quit()
+
+# ------------------ Area Calculation ------------------------------------------
+# Take the output and do the area calculation
+n_white_pixels = np.sum(img == 255) #sum the white pixels in the render
+
+proj_area = (n_white_pixels*(fitfactor**2)) / (250000) #scaling math for px/m^2
+
+# Print the details to the command line
+print("\n")
+print("FRONTAL AREA PROJECTION")
+print("-----------------------------------------------------------------------")
+print("Assumes STL in millimeters, area will be calculated in square meters.")
+print("Total number of triangles in STL: " + "\t" + str(triangles_count))
+print("\n")
+print("Projected Direction is in: " + "\t\t" + projaxis + " axis")
+print("\n")
+print("Projected Area: " + "\t\t\t" + str(proj_area) + " m^2")
+print("\n")
+print("Time to Compute: " + "\t\t\t" + str(time.process_time()) + " seconds")
+print("-----------------------------------------------------------------------")
+
+# ------------------ Show Window -----------------------------------------------
+if args.noshow is False:
+    print("FINISHED")
+    print("Close VTK window to exit...")
+    print("\n")
+    iren.Start() # Start the event loop to show the render
+
+else:
+    print("FINISHED")
+    print("\n")
+    quit()
